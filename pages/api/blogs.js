647fb644536317093e141986ede276f1e2e007ed
captainAyan <ayanchakraborty105@gmail.com>
2022-10-16 13:25:19 +0530

    fix: :bug: fixed bugs

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -3,13 +3,25 @@ import connectDb from "../../middleware/database";
 
 async function handler(req, res) {
   const { type } = req.query;
+  const page =
+    parseInt(req.query.page, 10) > 0 ? parseInt(req.query.page, 10) : 0;
+  const limit = 5;
 
   let blogs;
 
   if (type === "latest") {
-    blogs = await Blog.find().sort("-created_at").limit(4);
+    blogs = await Blog.find().sort("-created_at").select(["-content"]).limit(4);
   } else {
-    blogs = await Blog.find().sort("-created_at");
+    blogs = {
+      skip: page * limit,
+      limit: limit,
+      total: await Blog.find().count(),
+      blogs: await Blog.find()
+        .sort("-created_at")
+        .select(["-content"])
+        .skip(page * limit)
+        .limit(limit),
+    };
   }
 
   res.status(200).json(blogs);
--- pages/blogs.js
+++ pages/blogs.js
@@ -7,11 +7,31 @@ import styles from "../styles/Main.module.css";
 export default function Blogs(props) {
   const [blogs, setBlogs] = useState(props.blogs);
 
+  const [page, setPage] = useState(
+    parseInt(props.page, 10) ? parseInt(props.page, 10) : 0
+  );
+  const [limit, setLimit] = useState(parseInt(props.limit, 10));
+  const [total, setTotal] = useState(parseInt(props.total, 10));
+
   return (
     <div className={styles.container}>
       <div className={styles.blog_container}>
         <h1>Blogs</h1>
+        <p>
+          Page <span>{page + 1}</span> of{" "}
+          <span>{Math.ceil(total / limit)}</span>
+        </p>
         <div>
+          {blogs.length === 0 ? (
+            <center>
+              <h1
+                className={styles.description}
+                style={{ margin: "48px auto 72px auto" }}
+              >
+                No more Blogs
+              </h1>
+            </center>
+          ) : null}
           {blogs.map((blog, i) => {
             return (
               <BlogItem
@@ -24,15 +44,36 @@ export default function Blogs(props) {
             );
           })}
         </div>
+        <div>
+          <center>
+            <a
+              href={`?page=${page - 1}`}
+              style={{ fontStyle: "none" }}
+              className={styles.btn}
+            >
+              Prev
+            </a>
+            <span style={{ fontSize: "24px", margin: "auto 24px" }}>
+              {page + 1}
+            </span>
+            <a href={`?page=${page + 1}`} className={styles.btn}>
+              Next
+            </a>
+          </center>
+        </div>
       </div>
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const blogs = (await axios.get("http://localhost:3000/api/blogs")).data;
+export async function getServerSideProps(context) {
+  const page = context.query.page ? context.query.page : 0;
+
+  const { blogs, skip, limit, total } = (
+    await axios.get(`http://localhost:3000/api/blogs?page=${page}`)
+  ).data;
 
   return {
-    props: { blogs },
+    props: { blogs, page, skip, limit, total },
   };
 }
